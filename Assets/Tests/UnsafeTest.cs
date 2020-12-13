098adb1aade27d1b39d80d07fd262a062bcc326f
kazuki matsumoto <karasusan@gmail.com>
2020-12-13 18:50:12 +0900

    add nullable test

--- Assets/Tests/UnsafeTest.cs
+++ Assets/Tests/UnsafeTest.cs
@@ -47,6 +47,24 @@ public class UnsafeTest
         }
     }
 
+    struct TypeC<T> where T : unmanaged
+    {
+        public T? X;
+
+        public static unsafe explicit operator void*(TypeC<T> a)
+        {
+            int size = UnsafeUtility.SizeOf<TypeC<T>>();
+            var ptr = UnsafeUtility.Malloc(size, 4, Allocator.Temp);
+            UnsafeUtility.CopyStructureToPtr(ref a, ptr);
+            return ptr;
+        }
+        public static unsafe explicit operator TypeC<T>(void* ptr)
+        {
+            UnsafeUtility.CopyPtrToStructure(ptr, out TypeC<T> c);
+            return c;
+        }
+    }
+
     [Test]
     public unsafe void TypeATest()
     {
@@ -67,8 +85,48 @@ public class UnsafeTest
         AddGenericInt((void*)(IntPtr)a, (void*)(IntPtr)b, (void*)ptr); 
         var c = (TypeB<int>) ptr;
         Assert.AreEqual(a.X + b.X, c.X);
-    }   
-    
+    }
+
+    [Test]
+    public unsafe void TypeCTest1()
+    {
+        var a = new TypeC<int> { X = 1 };
+        var b = new TypeC<int> { X = 2 };
+        var ptr = UnsafeUtility.Malloc(UnsafeUtility.SizeOf<TypeC<int>>(), 4, Allocator.Temp);
+        AddGenericNullableInt((void*)a, (void*)b, ptr);
+        var c = (TypeC<int>)ptr;
+        Assert.AreEqual(a.X + b.X, c.X);
+        UnsafeUtility.Free(ptr, Allocator.Temp);
+    }
+
+    [Test]
+    public unsafe void TypeCTest2()
+    {
+        var a = new TypeC<long> { X = 1 };
+        var b = new TypeC<long> { X = 2 };
+        var ptr = UnsafeUtility.Malloc(UnsafeUtility.SizeOf<TypeC<long>>(), 4, Allocator.Temp);
+        AddGenericNullableLong((void*)a, (void*)b, ptr);
+        var c = (TypeC<long>)ptr;
+        Assert.AreEqual(a.X + b.X, c.X);
+        UnsafeUtility.Free(ptr, Allocator.Temp);
+    }
+
+    [Test]
+    public unsafe void TypeCTest3()
+    {
+        var a = new TypeC<int> { X = null };
+        var b = new TypeC<int> { X = null };
+        var ptr = UnsafeUtility.Malloc(UnsafeUtility.SizeOf<TypeC<long>>(), 4, Allocator.Temp);
+        AddGenericNullableInt((void*)a, (void*)b, ptr);
+        var c = (TypeC<int>)ptr;
+        Assert.False(c.X.HasValue);
+        UnsafeUtility.Free(ptr, Allocator.Temp);
+    }
+
     [DllImport(dllName)]
     static extern unsafe void AddGenericInt(void* ptr1, void* ptr2, void* ptr3);
+    [DllImport(dllName)]
+    static extern unsafe void AddGenericNullableInt(void* ptr1, void* ptr2, void* ptr3);
+    [DllImport(dllName)]
+    static extern unsafe void AddGenericNullableLong(void* ptr1, void* ptr2, void* ptr3);
 }
--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -1,3 +1,10 @@
+#include <cstdint>
+
+#if _MSC_VER // this is defined when compiling with Visual Studio
+#define EXPORT_API __declspec(dllexport) // Visual Studio needs annotating exported functions with this
+#else
+#define EXPORT_API // XCode does not need annotating exported functions, so define is empty
+#endif
 
 template<typename T>
 struct C
@@ -5,10 +12,43 @@ struct C
     T X;
 };
 
+template<typename T>
+struct Nullable
+{
+    T value;
+    bool hasValue;
+
+	Nullable<T> operator +(const Nullable<T>& a) const
+	{
+        Nullable<T> c;
+        if(!this->hasValue && !a.hasValue)
+        {
+            c.hasValue = false;
+            c.value = 0;
+            return c;
+        }
+		
+        c.hasValue = true;
+        c.value = (this->hasValue ? this->value : 0) + (a.hasValue ? a.value : 0);
+        return c;
+	}
+};
+
 extern "C"
 {
-    __stdcall void AddGenericInt(const C<int>* a, const C<int>* b, C<int>* c)
-    {
-        c->X = a->X + b->X;
-    }
+EXPORT_API void AddGenericInt(const C<int32_t>* a, const C<int32_t>* b, C<int32_t>* c)
+{
+    c->X = a->X + b->X;
+}
+
+EXPORT_API void AddGenericNullableInt(const C<Nullable<int32_t>>* a, const C<Nullable<int32_t>>* b, C<Nullable<int32_t>>* c)
+{
+    c->X = a->X + b->X;
+}
+
+EXPORT_API void AddGenericNullableLong(const C<Nullable<int64_t>>* a, const C<Nullable<int64_t>>* b, C<Nullable<int64_t>>* c)
+{
+    c->X = a->X + b->X;
+}
+
 }
